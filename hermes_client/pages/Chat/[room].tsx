47744dc8b1a2d31388208d23d97f5ddf5e68bc66
George <gngo2018@gmail.com>
2022-03-24 00:37:45 -0400

    Added socket emit functionality

--- hermes_client/pages/Chat/[room].tsx
+++ hermes_client/pages/Chat/[room].tsx
@@ -7,29 +7,29 @@ import chatStyles from './chat.module.css'
 
 const socket = io(process.env.NEXT_PUBLIC_hermes_api_url!);
 
-export default function Room(){
-    const messageLogArray= [
-        {id: 1, messageSender: 'George', message:'So you decided to come and chat. What would you like to talk about?', profileColor: 'red'},
-        {id: 2, messageSender: 'Kyle', message:'Why is the earth flat?', profileColor: 'gray'},
-        {id: 3, messageSender: 'Sebastian', message:'Why do cows moo?', profileColor:'blue'},
-        {id: 4, messageSender: 'Cortana', message:'I think I am in the wrong chat...', profileColor:'var(--purple)'}
+export default function Room() {
+    const messageLogArray = [
+        { id: 1, messageSender: 'George', message: 'So you decided to come and chat. What would you like to talk about?', profileColor: 'red' },
+        { id: 2, messageSender: 'Kyle', message: 'Why is the earth flat?', profileColor: 'gray' },
+        { id: 3, messageSender: 'Sebastian', message: 'Why do cows moo?', profileColor: 'blue' },
+        { id: 4, messageSender: 'Cortana', message: 'I think I am in the wrong chat...', profileColor: 'var(--purple)' }
     ]
     const router = useRouter();
-    const {room} = router.query;
-    const {setPageTitle} = usePageTitleContext();
+    const { room } = router.query;
+    const { setPageTitle } = usePageTitleContext();
     const [message, setMessage] = useState('');
     const [messageLog, setMessageLog] = useState(messageLogArray);
 
 
-    function SubmitMessage(){
-        if(message !== '') {
+    function SubmitMessage() {
+        if (message !== '') {
             const messageObj = {
                 id: 5,
                 messageSender: 'Guest',
                 message: message,
                 profileColor: 'green'
             };
-    
+
             const inMemoryMessageLog = messageLog;
             setMessageLog([...inMemoryMessageLog, messageObj]);
             socket.emit('howdy', message);
@@ -37,22 +37,42 @@ export default function Room(){
         }
     }
 
-    useEffect(()=>{
-        if(room){
+    useEffect(() => {
+        if (room) {
             setPageTitle(room.toString());
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [room]);
 
-    useEffect(() =>{
+    useEffect(() => {
         console.log(socket);
-        
+
         socket.on('hello', (arg) => {
             console.log('Message from server:', arg);
         });
     }, [])
 
-    return(
+    useEffect(() => {
+        socket.on('chat-response', msg => {
+            console.log(msg);
+            const id = socket.id;
+            const messageObj = {
+                id: 6,
+                messageSender: id,
+                message: msg,
+                profileColor: 'red'
+            };
+
+            console.log(messageObj);
+
+
+            const inMemoryMessageLog = messageLog;
+            setMessageLog([...inMemoryMessageLog, messageObj]);
+            setMessage('');
+        })
+    });
+
+    return (
         <div className={chatStyles.room_container}>
             <Head>
                 <title>Chat Room</title>
@@ -62,21 +82,21 @@ export default function Room(){
                     messageLog.map((message, index) =>
                         <div key={index}>
                             {
-                                message.id !== 5 ? 
-                                <div className={chatStyles.message_flex_container}>
-                                    <div className={chatStyles.message_sender_image} style={{background:`${message.profileColor}`}}></div>
-                                    <div className={chatStyles.message_sender_details}>
-                                        <div>{message.messageSender}</div>
-                                        <div>{message.message}</div>
+                                message.id !== 5 ?
+                                    <div className={chatStyles.message_flex_container}>
+                                        <div className={chatStyles.message_sender_image} style={{ background: `${message.profileColor}` }}></div>
+                                        <div className={chatStyles.message_sender_details}>
+                                            <div>{message.messageSender}</div>
+                                            <div>{message.message}</div>
+                                        </div>
+                                    </div> :
+                                    <div className={chatStyles.user_message_flex_container}>
+                                        <div className={chatStyles.user_details}>
+                                            {/* <div>{message.messageSender}</div> */}
+                                            <div>{message.message}</div>
+                                        </div>
+                                        <div className={chatStyles.user_image} style={{ background: `${message.profileColor}` }}></div>
                                     </div>
-                                </div> : 
-                                <div className={chatStyles.user_message_flex_container}>
-                                    <div className={chatStyles.user_details}>
-                                        {/* <div>{message.messageSender}</div> */}
-                                        <div>{message.message}</div>
-                                    </div>
-                                    <div className={chatStyles.user_image} style={{background:`${message.profileColor}`}}></div>
-                                </div>
                             }
                         </div>
                     )
